051f77f38319218c653f2c78bc9f2c44fed171d7
Paul Shahid <noreply@bukket>
2018-11-29 21:51:47 -0500

    Adding correct character limit

--- fn.py
+++ fn.py
@@ -47,6 +47,9 @@ def get_bot_id():
         return bot_id
 
 
+# Minimum character limit for all messages
+CHAR_LIMIT = 5
+
 sc = SlackClient(get_token())
 
 
@@ -62,20 +65,22 @@ def bukket_event(request):
     
     if request_json.get('type') == 'url_verification':
         challenge = request_json['challenge']
+        logging.info('Returning response for slack challenge-response.')
         return jsonify({"challenge": challenge})
     else:  # This is a message to a channel
         evt = request_json['event']
         txt = evt.get('text', '')
-        if 250 > len(txt) > 0:
-            return 'ok'
         if evt['channel_type'] == 'channel' and evt['channel'] == channel and evt.get('bot_id', '') != bot_id:
-            if len(txt) > 250 or len(txt) == 0:
+            logging.info(evt)
+            if len(txt) > 250 or len(txt) < CHAR_LIMIT:
+                logging.warn(f"Text character limit exceeds bounds, got {len(txt)}.")
                 return 'ok'
             txt = urllib.parse.quote(txt.lower())
             response = requests.get(f"{url}/factoid/search/{txt}")
             if response.status_code == 200:
                 response_data = response.json()['data']
                 if len(response_data) == 0:
+                    logging.info("Got no response data from bukket core api.")
                     return 'ok'
                 idx = random.randint(0, len(response_data)-1)
                 try:
@@ -84,8 +89,8 @@ def bukket_event(request):
                         channel=channel,
                         text=response_data[idx]['tidbit']
                     )
+                    logging.info("Posted tidbit to slack")
                 except IndexError:
                     logging.warn("IndexError encountered when getting random fact.")
                     return 'ok'
-        logging.info(evt)
         return 'ok'
